a5306ce709e558ffcaea8e565536279127256a17
Frank <frankli0324@hotmail.com>
2022-02-03 21:28:26 +0800

    string serialization

--- javaserialize/constants.py
+++ javaserialize/constants.py
@@ -1,17 +1,18 @@
 from .types import *
 
+MAX_BLOCK_SIZE = 1024
 STREAM_MAGIC = bytes((0xac, 0xed))
 STREAM_VERSION = bytes((0x00, 0x05))
 TC_TYPES = {
-    type(None): bytes((0x70)),
-    ref: bytes((0x71)),
-    classdesc: bytes((0x72)),
-    str: bytes((0x74)),
-    list: bytes((0x75)),
-    type(type(None)): bytes((0x76)),  # Reference to Class # ?
+    type(None): bytes((0x70,)),
+    ref: bytes((0x71,)),
+    classdesc: bytes((0x72,)),
+    str: bytes((0x74,)),
+    list: bytes((0x75,)),
+    type(type(None)): bytes((0x76,)),  # Reference to Class # ?
     # BLOCKDATA: 0x77; ENDBLOCKDATA: 0x78
-    reset: bytes((0x79)),
+    reset: bytes((0x79,)),
     # BLOCKDATALONG: 0x7a
-    wexception: bytes((0x7b)),
-    longstr: bytes((0x7c)),
+    wexception: bytes((0x7b,)),
+    longstr: bytes((0x7c,)),
 }
--- javaserialize/serialize.py
+++ javaserialize/serialize.py
@@ -1,15 +1,53 @@
-import io
+from typing import Union
+from io import BytesIO
 
 from .types import *
 from .constants import *
 
 
-def serialize_stream(obj, stream=None):
+def _serialize_str(s: Union[str, bytes], stream: BytesIO):
+    if type(s) == str:
+        s = s.encode('utf-8')
+    if len(s) <= 0xffff:
+        stream.write(TC_TYPES[str])
+        # print((len(s) >> 8, len(s) >> 0))
+        stream.write(bytes((len(s) >> 8, len(s) >> 0)))
+    else:
+        stream.write(bytes(
+            (len(s) >> (56 - 8 * i) & 0xff for i in range(8))
+        ))
+        stream.write(TC_TYPES[longstr])
+    pos = 0
+    for i in range(len(s)):
+        if s[i] >> 4 in (15, 16):
+            # see javadoc /docs/api/java.base/java/io/DataInput.html
+            raise ValueError()
+        if s[i] == 0:
+            stream.write(s[pos:i - 1])
+            pos = i + 1
+            stream.write(bytes((0xc0, 0x80)))
+    stream.write(s[pos:])
+    stream.flush()
+
+
+_handles = {
+    str: _serialize_str,
+    bytes: _serialize_str,
+}
+
+
+def _serialize(obj, stream: BytesIO, unshared):
+    if type(obj) not in _handles:
+        pass
+    _handles[type(obj)](obj, stream)
+
+
+def serialize_stream(obj, stream: BytesIO = None):
     if not stream:
-        stream = io.BytesIO()
+        stream = BytesIO()
     stream.write(STREAM_MAGIC)
     stream.write(STREAM_VERSION)
-
+    _serialize(obj, stream, False)
     return stream
 
 
